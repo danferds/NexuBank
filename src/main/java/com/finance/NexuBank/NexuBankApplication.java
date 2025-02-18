ddc6c1db9202c981a9a0c61c4634fdee81517d2b
Daniel Fernandes <danielferdsc@gmail.com>
2025-02-17 23:38:23 -0300

    Configurações iniciais.

---
+++ src/main/java/com/finance/NexuBank/NexuBankApplication.java
@@ -0,0 +1,14 @@
+package com.finance.NexuBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuBankApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/finance/NexuBank/NexuBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.NexuBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
