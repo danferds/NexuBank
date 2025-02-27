b5ac2444b6ed4365433c658838d4e0e6817c7e63
Daniel Fernandes <danielferdsc@gmail.com>
2025-02-27 17:24:34 -0300

    Ajusta configurações do projeto e renomeia pacotes

--- src/main/java/com/finance/NexuBank/NexuBankApplication.java
+++ src/main/java/com/finance/NexuBank/NexuBankApplication.java
@@ -1,4 +1,4 @@
-package com.finance.NexuBank;
+package com.finance.nexubank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,4 +11,3 @@ public class NexuBankApplication {
 	}
 
 }
-
--- src/test/java/com/finance/NexuBank/NexuBankApplicationTests.java
+++ src/test/java/com/finance/NexuBank/NexuBankApplicationTests.java
@@ -1,4 +1,4 @@
-package com.finance.NexuBank;
+package com.finance.nexubank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
